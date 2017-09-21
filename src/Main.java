d5a9f3b8ffe7bc4e1a7d61e364b5323deba47b2d
Mikkel Sørensen <mik4211@edu.easj.dk>
2017-09-21 13:01:35 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+
+       // Vi definerer minutter og sekunder som integer, da det er hele tal vi bruger.
+        int minutterRes;
+        int sekunderRes;
+
+
+        // Scanner hvor sekunder kan indtastes
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter seconds here");
+
+        int antalSekunder = input.nextInt();
+
+
+        // Selve beregningen
+        minutterRes = antalSekunder/60;
+        sekunderRes = antalSekunder%60;
+
+        // Her skrives resultatet af udregningen
+        System.out.println("Minutter: " + minutterRes + " Sekunder: " + sekunderRes);
+
+
+    }
+}
